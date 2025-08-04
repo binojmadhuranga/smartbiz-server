7017c8114e8234cc9e7bc9cb3bed26d352b6f956
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-08-04 12:56:48 +0530

    Start the Project

---
+++ smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/SmartbizApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartbiz.smartbiz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbizApiApplication.class, args);
+	}
+
+}
---
+++ smartbiz-api/src/test/java/com/smartbiz/smartbiz_api/SmartbizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbiz.smartbiz_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
