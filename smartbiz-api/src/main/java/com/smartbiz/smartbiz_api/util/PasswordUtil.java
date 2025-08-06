ebd71f437a166fe8268d6133a094f5faaa79c4b1
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-08-06 22:02:40 +0530

    Implement the password hashing using spring-security-crypto

---
+++ smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/util/PasswordUtil.java
@@ -0,0 +1,15 @@
+package com.smartbiz.smartbiz_api.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtil {
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public static String hash(String password) {
+        return encoder.encode(password);
+    }
+
+    public static boolean matches(String rawPassword, String hashedPassword) {
+        return encoder.matches(rawPassword, hashedPassword);
+    }
+}
