0f30aaaf54fb4af06ce72e9ed638cc74217a2f1b
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-09-09 17:06:28 +0530

    improve readability.

--- smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/config/WebConfig.java
+++ smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.smartbiz.smartbiz_api.config;
 import com.smartbiz.smartbiz_api.interceptor.JwtAuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,4 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/customers/**", "/api/items/**", "/api/suppliers/**")
                 .excludePathPatterns("/api/auth/**", "/api/public/**"); // adjust to your public endpoints
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // allow all /api paths
+                .allowedOrigins("http://localhost:5173") // frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
 }
