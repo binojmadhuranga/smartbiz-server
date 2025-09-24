02373486b655d4deed8a903b5cb5b3a8b80604fe
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-09-24 09:52:17 +0530

    Implment the function for admin and view / download  users uploaded file.

--- smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/controller/AdminPlaneController.java
+++ smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/controller/AdminPlaneController.java
@@ -1,8 +1,10 @@
 package com.smartbiz.smartbiz_api.controller;
 import com.smartbiz.smartbiz_api.dto.PlanUpdateRequestDto;
+import com.smartbiz.smartbiz_api.entity.Payment;
 import com.smartbiz.smartbiz_api.service.AdminPlanService;
 import com.smartbiz.smartbiz_api.service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,4 +25,16 @@ public class AdminPlaneController {
         return ResponseEntity.ok(adminService.updateUserPlan(userId, request).getId());
     }
 
+    @GetMapping("/users/{userId}/payments/download")
+    public ResponseEntity<byte[]> downloadLatestUserPaymentSlip(
+            @PathVariable Long userId
+    ) throws Exception {
+        Payment payment = paymentService.getLatestPaymentByUserId(userId);
+        byte[] fileData = paymentService.downloadPaymentFile(payment.getId());
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"" + payment.getOriginalFilename() + "\"")
+                .contentType(MediaType.parseMediaType(payment.getContentType()))
+                .body(fileData);
+    }
 }
