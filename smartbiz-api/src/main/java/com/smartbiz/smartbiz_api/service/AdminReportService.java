85ca9e2b6caaed699cbadb161e62f8a5dbad47c9
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-09-29 15:33:36 +0530

    update admin report genaration  mehtod.

--- smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/service/AdminReportService.java
+++ smartbiz-api/src/main/java/com/smartbiz/smartbiz_api/service/AdminReportService.java
@@ -2,8 +2,11 @@ package com.smartbiz.smartbiz_api.service;
 
 import com.smartbiz.smartbiz_api.dto.AdminReportDto;
 
+import java.io.ByteArrayInputStream;
+
 public interface AdminReportService {
 
     AdminReportDto generateUserReport();
 
+    ByteArrayInputStream generateUserReportPdf();
 }
